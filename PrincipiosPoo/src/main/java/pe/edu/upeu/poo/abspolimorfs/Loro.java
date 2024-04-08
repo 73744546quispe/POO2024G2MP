089072126556360e05da9af7583e9b682a13e8c4
73744546quispe <jhuacaniquispe9@gamil.com>
2024-04-08 10:34:30 -0500

    agregando archivo

---
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/abspolimorfs/Loro.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.abspolimorfs;
+
+
+public class Loro extends Animal{
+
+    @Override
+    public void animalSound() {
+        System.out.println("Esto es Polimorfismo entiende pue!!");
+  
+    
+    }
+    
+   
+    @Override
+    public void sleep(){
+        System.out.println("Esta clase no me gusta me da Zzz zz..");
+    }
+    
+}
---
+++ PrincipiosPoo/src/main/java/pe/edu/upeu/poo/polimorfismo/Loro.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.poo.polimorfismo;
+
+/**
+ *
+ * @author Datos
+ */
+public class Loro extends Animal {
+    
+       //aplicacion de concepto de polimorfismo
+    @Override
+    public void sonidoAnimal(){
+        System.err.println("Hola Manito..Aprende....Aprende....Esfuerzate");
+    }
+}
